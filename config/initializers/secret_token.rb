659788b0b0263a7b4de731d6d35ba9623f5c5117
Nick Meehan <nnmeehan@gmail.com>
2014-08-22 23:17:57 -0600

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotHouse::Application.config.secret_key_base = '00dbceda84cb73674fc02eb8f835e21cb69c00f856c7d39f04f86aef3cbb6e1a61ba6a135db20824c7fde1bf671c9956f24049c6bc2b15a7082a745895f70759'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HotHouse::Application.config.session_store :cookie_store, key: '_hot_house_session'
