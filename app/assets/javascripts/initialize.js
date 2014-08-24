99fd71ebba955d5bafe99a798a4b25d0f225707d
ChrisBacon2 <chrisbacon@Chriss-MacBook-Pro.local>
2014-08-24 14:38:33 -0600

    updating navbar and homepage

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   var mapController = new MapController(new MapView())
   mapController.init()
-  google.maps.event.addDomListener(window, 'load', initialize);
 })
 
 
